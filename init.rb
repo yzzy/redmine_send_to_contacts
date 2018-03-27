4834787bb70fcdd706305b0963883209fdf92166
yzzy <yzzy@yzzy.org>
2018-03-27 13:45:33 +0500

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_send_to_contacts do
+  name 'Redmine Send To Contacts plugin'
+  author 'Alexander Abramov'
+  description 'This is a Redmine plugin for sending emails to contacts'
+  version '0.0.1'
+  url 'https://github.com/yzzy/redmine_send_to_contacts'
+  author_url 'https://github.com/yzzy'
+end
